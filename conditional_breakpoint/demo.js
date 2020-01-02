1690fe2f8e0ba06dcc510670bb91d8510afe0f9a
dudueasy <dudueasy@126.com>
2020-01-03 00:07:41 +0800

    feat:  rearrange conditional breakpoint demo

--- conditional_breakpoint/demo.js
+++ conditional_breakpoint/demo.js
@@ -1,8 +1,5 @@
-// 将这个文件复制到 chrome snippets 中运行
-script = document.createElement('script');
-script.src = 'https://cdn.bootcss.com/blueimp-md5/2.12.0/js/md5.js';
-
-document.body.appendChild(script)
+console.log('start debugging')
+debugger
 
 // 通过 debugger 来获得 i 为 8971 时的 md5 值
 for(let i = 0; i< 10000 ; i++){
